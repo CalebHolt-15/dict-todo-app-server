3d8438493fe558d83741e4d2a4951d9b84e00001
CalebHolt-15 <calebholt100@gmail.com>
2021-08-27 01:13:32 +0530

    Initialise routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import "dotenv/config"
+import "dotenv/config" //load env variables from .env
 import express from "express"
 import mongoose from "mongoose"
+import todolistRouter from "./routes/todolist.router.js" //wontwork w/o .js extnsn
 
 const app = express()
 
@@ -8,5 +9,9 @@ mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on("error", (error) => console.error(error))
 db.once("open", () => console.log("Connected DB"))
+
+app.use(express.json()) //allowservertoaccept json asabody insideGET/POST/..element
+
+app.use("/todo", todolistRouter)
 
 app.listen(8090, () => console.log("Server Started"))
