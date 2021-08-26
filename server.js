164ffc1556f62ef4f4d24e8866290592fffba28e
CalebHolt-15 <calebholt100@gmail.com>
2021-08-27 00:18:35 +0530

    Initialise DB Connection

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+import "dotenv/config"
 import express from "express"
+import mongoose from "mongoose"
 
 const app = express()
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on("error", (error) => console.error(error))
+db.once("open", () => console.log("Connected DB"))
 
 app.listen(8090, () => console.log("Server Started"))
